900f9b2c36befab34ae8582f6cf5d8a6f8b6b4a8
Katia Prigon <katiapr89@gmail.com>
2017-04-09 18:40:43 +0200

    Add files via upload

---
+++ ScriptLogin.js
@@ -0,0 +1,60 @@
+﻿function loadPage() {
+    $("#txtSport").hide();
+    $("#txtAboutUs").hide();
+    $("#txtRecipes").hide();
+    $("iframe").hide();
+    cmdAboutUs = $("#AboutUs").click(clickListener);
+    cmdSport = $("#Sport").click(clickListener);
+    cmdRecipes = $("#Recipes").click(clickListener);
+
+}
+var showTxt = function (num) {
+    showTxt[0] = "Hey you!\n\n Let us tell you about us!\n This is a very useful and friendly interface\n" +
+                    "It can help you to reduce your wright, upper it or sipmly save it!\n" +
+                    "All you need to do is only Sign up and Begin your session!\n" +
+                    "You can conntact us by clicking on our names at the end of the page\n\n\n" +
+                    "Enjoy!\n\nEasyWayght!:)";
+    showTxt[1] = "Wellcome to our sport collection of links!\n" +
+                    "Here you can read articles and find useful information about exercises\n\n\n" +
+                      "Enjoy!\nEasyWayght!:)";
+    showTxt[2] = "Wellcome to our recipes collection of links!\n" +
+                    "Here you can find information about healthy food,\n" +
+                    "some recipes and more useful information\n\n\n" +
+                      "Enjoy!\nEasyWayght!:)";
+}
+
+////Get
+//var bla = $('#txt_name').val();
+
+////Set
+//$('#txt_name').val('bla');
+var clickListener = function (e) {
+    var txt = document.getElementById("classTxt");
+
+    if (e.target.id == "AboutUs") {
+        $("iframe").hide();
+        alert("AboutUs");
+        $("iframe").show();
+        txt.innerText = showTxt(0);
+
+
+    }
+    if (e.target.id == "Sport") {
+        $("iframe").hide();
+        alert("Sport");
+        $("iframe").show();
+        txt.innerText = showTxt(1);
+
+    }
+    if (e.target.id == "Recipes") {
+        $("iframe").hide();
+        alert("Recipes");
+        $("iframe").show();
+        txt.innerText = showTxt(2);
+
+    }
+    /*
+    *
+    var content = myIFrame.contentWindow.document.body.innerHTML;
+    */
+}
---
+++ ScriptPersonal.js
@@ -0,0 +1,138 @@
+﻿//GLOBAL//
+var sumOfCalories;
+var userCalories;
+///////////////////////////////////////////////////////////////
+
+var myDiary = function () {
+    var initModule = function () {
+        alert("init Module");
+        $("#cmdAdd").click(add);
+        $("#cmdEdit").click(edit);
+        $("#cmdRemove").click(remove);
+        $("#selectAll").click(selectAll);
+        fillTableItems();
+    };
+   
+    var fillTableItems = function () {
+        if (localStorage.getItem("Items") == undefined)
+            return;
+
+        $("tr:gt(0)").remove();
+
+        var items = JSON.parse(localStorage.getItem("Items"));
+        console.log(items);
+        $.each(items, function (i, item) {
+            var row;
+            if (i % 2 == 0) {
+                row = "<tr class = 'even'>";
+            }
+            else {
+                row = "<tr class = 'odd'>";
+            }
+            row += "<td><input id='selectOne' type='checkbox'></td>" +
+                "<td>" + item.date + "</td>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.calories + "</td>" +
+                "<td>" + item.marks + "</td>" +
+                "</tr>";
+
+            $("tr:last").after(row);
+        });
+        alert("after each");
+    };
+    ///////////////////////////////////
+    var selectAll = function (e) {
+        var table = $(e.target).closest('table');
+        $('td input:checkbox', table).prop('checked', this.checked);
+    };
+
+    ///////////////////////////////////
+    var add = function () {
+        alert("add finc");
+        var date = $("#txtDate").val();
+        var name = $("#txtItem").val();
+        var cal = $("#txtCalories").val();
+        var marks = $("#txtMarks").val();
+        var item = { date: date, name: name, calories: cal, marks: marks };
+        addItem(item);
+        fillTableItems();
+    }
+
+    var remove = function () {
+        var i = 0;
+        if (document.getElementById("selectAll").checked == true) {
+            if (confirm("האם אתה בטוח שברצונך למחוק את הכל?") == true) {
+                if (localStorage.getItem("Items") != undefined)
+                    localStorage.removeItem("Items");
+                localStorage.setItem("Items", JSON.stringify([]));
+                fillTableItems();
+                document.getElementById("selectAll").checked = false;
+            }
+        }
+        else {
+            $('input:checkbox').each(function () {
+                if ($(this).is(':checked')) {
+                    if(confirm(" האם אתה בטוח שברצונך למחוק פריט זה?")==true)
+                    {
+                        alert("inside remove item " + i);
+                        var itemsStr = localStorage.getItem("Items");
+                        var itemsObj = JSON.parse(itemsStr);
+                        itemsObj.splice(i-1, 1);
+                        localStorage.setItem("Items", JSON.stringify(itemsObj));
+                        itemsStr = localStorage.getItem("Items");
+                        fillTableItems();
+                       // console.log(itemsStr);
+                       // fromLocalstrToList();
+                    }
+                }
+                i += 1;
+            })
+
+        }
+    };
+
+    var edit = function () {
+        alert("edit");
+        var i = 0;
+        $('input:checkbox').each(function () {
+            if ($(this).is(':checked')) {
+                alert("checked");
+                if (confirm(" האם אתה בטוח שברצונך לעדכן את הפריט? ") == true) {
+                    var itemsStr = localStorage.getItem("Items");
+                    var itemsObj = JSON.parse(itemsStr);
+                   
+                    var newDate = $("#txtDate").val();
+                    var newName = $("#txtItem").val();
+                    var newCal = $("#txtCalories").val();
+                    var newMarks = $("#txtMarks").val();
+                    
+                    if (newDate != "")
+                        itemsObj[i - 1].date = newDate;
+                    if (newName != "")
+                        itemsObj[i - 1].name = newName;
+                    if (newCal != "")
+                        itemsObj[i - 1].calories = newCal;
+                    if (newMarks != "")
+                        itemsObj[i - 1].marks = newMarks;
+
+                    localStorage.setItem("Items", JSON.stringify(itemsObj));
+                    fillTableItems();
+                }
+            }
+            i += 1;
+        })
+    };
+
+    var addItem = function (item) {
+        var itemsStr = localStorage.getItem("Items");
+        var itemsObj = JSON.parse(itemsStr) || [];
+        itemsObj.push(item);
+        localStorage.setItem("Items", JSON.stringify(itemsObj));
+    };
+
+    return {
+        initModule: initModule,
+    };
+}();
+
+$(document).ready(myDiary.initModule);
